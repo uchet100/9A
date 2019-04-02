917a1ec1365147ef8a4e981a3bf9ac01a7c56011
tohan <up853094@myport.ac.uk>
2019-04-02 23:59:45 +0100

    Updated pay page

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,14 @@ const button = document.querySelector('button')
 const input = document.getElementById('item')
 let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : []  //items array to pass to local storage
 
-//using JSON.stringify to convert data array into strings 
+//using JSON.stringify to convert data array into strings
 //using JSON.pase to convert what localStorage passes (local storage only outputs values) in usable data
 localStorage.setItem('items', JSON.stringify(itemsArray))
 const data = JSON.parse(localStorage.getItem('items'))
 
-// creates the list (list element), sets the text of the 
+
+
+// creates the list (list element), sets the text of the
 //element to the parameter set previously and appends it to the ul
 const liMaker = text => {
   const li = document.createElement('li')
@@ -17,15 +19,15 @@ const liMaker = text => {
   ul.appendChild(li)
 }
 
-//prevents the default procedure for the submit event that would send data to server 
+//prevents the default procedure for the submit event that would send data to server
 //using local storage = no server involved
 form.addEventListener('submit', function(e) {
   e.preventDefault()
 
   itemsArray.push(input.value)
   localStorage.setItem('items', JSON.stringify(itemsArray))
-  //calling function that makes the item and adds it to the DOM 
-  //then sets the empty string so the form is empty again 
+  //calling function that makes the item and adds it to the DOM
+  //then sets the empty string so the form is empty again
   liMaker(input.value)
   input.value = ''
  })
@@ -36,7 +38,7 @@ data.forEach(item => {
 })
 
 
-//refresh of the array and updates frontend 
+//refresh of the array and updates frontend
 button.addEventListener('click', function() {
   localStorage.clear()
   while (ul.firstChild) {
@@ -48,24 +50,25 @@ button.addEventListener('click', function() {
 function submit(){
   var submit = document.getElementsByName('submit');
   submit.addEventListener("onClick",process,false);
+
+
 }
 
 
-// small test for calculating input values
-//needs the local storage scripts to be adapted and keep the calculated results in a list
-//proper tax calculation needs to be implemented to page-script
 function process() {
   var payVar= parseFloat(document.calculate.pay.value)
   var hourVar = parseFloat(document.calculate.hour.value)
+  var taxVar = parseFloat(document.calculate.tax.value)
   var salaryVar = payVar * hourVar
   document.calculate.salary.value = salaryVar;
-
+  var taxesVar = (salaryVar * taxVar)/100
+  document.calculate.taxes.value = taxesVar;
+  var netPayVar = salaryVar - taxesVar
+  document.calculate.income.value = netPayVar;
+  
 
   if(!isNaN(result)){
     document.getElementById("result").innerHTML = result;
     }
-
-
-
 
   }
